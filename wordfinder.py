b18dfa89c53e695dbcb1eb857827d15ff4fe5609
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-25 16:17:14 -0800

    Wrote doctest, init, readFile

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,23 @@
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+
+    >>> wf = WordFinder("test_words.txt")
+    3 words read
+    >>> wf.words
+    ['cat', 'dog', 'porcupine']
+    """
+
+    def __init__(self, filepath):
+        """Create Word Finder by reading in words from filepath"""
+        self.words = WordFinder.readFile(filepath)
+        print(f"{len(self.words)} words read")
+
+    @staticmethod
+    def readFile(filepath):
+        """Reads a file and returns list of the file lines"""
+        words = []
+        file = open(filepath, "r")
+        for line in file:
+            words.append(line.strip())
+        file.close()
+        return words
