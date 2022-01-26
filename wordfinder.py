f0e805ee5c9dd1fffce6e61b417f1dcd4c60b81d
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-25 17:07:06 -0800

    Finished wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary.
 
@@ -11,37 +12,47 @@ class WordFinder:
 
     def __init__(self, filepath):
         """Create Word Finder by reading in words from filepath"""
-        self.words = self.readFile(filepath)
+
+        self.words = self._readFile(filepath)
         print(f"{len(self.words)} words read")
 
-    
-    def readFile(self, filepath):
+    def _readFile(self, filepath):
         """Reads a file and returns list of the file lines"""
+
         words = []
         file = open(filepath, "r")
         for line in file:
-            words.append(line.strip())
+            if self._isValid(line):
+                words.append(line.strip())
         file.close()
         return words
 
+    def _isValid(self, line):
+        """Determines if line should be included. Always returns True."""
+
+        return True
+
     def random(self):
         """Returns random word"""
+
         return choice(self.words)
 
 
 class SpecialWordFinder(WordFinder):
-    """"""
+    """SpecialWordFinder: WordFinder that excludes special cases.
+
+    >>> wf = SpecialWordFinder("special_words.txt")
+    4 words read
+    >>> wf.words
+    ['kale', 'parsnips', 'apple', 'mango']
+    """
 
     def __init__(self, filepath):
+        """Create Special Word Finder which is type of Word Finder"""
+
         super().__init__(filepath)
 
-    
-    def readFile(self, filepath):
-        words = []
-        file = open(filepath, "r")
-        for line in file:
-            if line.startswith("#") or line == "\n":
-                continue
-            words.append(line.strip())
-        file.close()
-        return words
+    def _isValid(self, line):
+        """Returns False for lines that start with '#' or blank lines"""
+
+        return not (line.startswith("#") or line == "\n")
