2cf4489db3eaeb94153ae629fcab0645c3e3a9c9
= <stevenmtang1@gmail.com>
2022-01-25 16:38:03 -0800

    completed wordfinder, WIP: add docstrings

--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,5 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary.
 
@@ -9,15 +11,37 @@ class WordFinder:
 
     def __init__(self, filepath):
         """Create Word Finder by reading in words from filepath"""
-        self.words = WordFinder.readFile(filepath)
+        self.words = self.readFile(filepath)
         print(f"{len(self.words)} words read")
 
-    @staticmethod
-    def readFile(filepath):
+    
+    def readFile(self, filepath):
         """Reads a file and returns list of the file lines"""
         words = []
         file = open(filepath, "r")
         for line in file:
+            words.append(line.strip())
+        file.close()
+        return words
+
+    def random(self):
+        """Returns random word"""
+        return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """"""
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    
+    def readFile(self, filepath):
+        words = []
+        file = open(filepath, "r")
+        for line in file:
+            if line.startswith("#") or line == "\n":
+                continue
             words.append(line.strip())
         file.close()
         return words
